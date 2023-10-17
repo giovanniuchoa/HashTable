10d21c60bc3830526836433eb265ec778c330be6
João Gregorini <75985123+jvg21@users.noreply.github.com>
2023-10-17 20:43:46 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
         HashTable<Integer, String> hashTable = new HashTable<>(16, 0.75);
 
         // Inserção
-        hashTable.put(1, "Alice");
+        hashTable.put(1, "Joao");
         System.out.println("Insere (1) - " + hashTable.get(1));
-        hashTable.put(2, "Bob");
+        hashTable.put(2, "Giovanni");
         System.out.println("Insere (2) - " + hashTable.get(2));
-        hashTable.put(3, "Charlie");
+        hashTable.put(3, "Terezin");
         System.out.println("Insere (3) - " + hashTable.get(3));
 
         // Busca
-        System.out.println("Busca (2): " + hashTable.get(2)); // Deve imprimir "Bob"
+        System.out.println("Busca (2): " + hashTable.get(2)); 
 
         // Remoção
-        System.out.println("Removido (1): " + hashTable.remove(1)); // Deve imprimir "Alice"
+        System.out.println("Removido (1): " + hashTable.remove(1)); 
 
         // Teste de eficiência
         long startTime = System.nanoTime();
